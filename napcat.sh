258e22911bd488ca9feaa273e6b10b84d31981cb
initialencounter <2911583893@qq.com>
2024-06-16 20:45:34 +0800

    fix syntax error near unexpected token `newline'

--- napcat.sh
+++ napcat.sh
@@ -2,31 +2,10 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # 安装必要的软件包
-apt-get update && apt-get install -y \
-    libnss3 \
-    libnotify4 \
-    libsecret-1-0 \
-    libgbm1 \
-    libasound2 \
-    fonts-wqy-zenhei \
-    gnutls-bin \
-    libglib2.0-dev \
-    libdbus-1-3 \
-    libgtk-3-0 \
-    libxss1 \
-    libxtst6 \
-    libatspi2.0-0 \
-    libx11-xcb1 \
-    ffmpeg \
-    unzip \
-    jq \
-    curl && \
-    apt autoremove -y && \
-    apt clean && \
-    rm -rf \
-    /var/lib/apt/lists/* \
-    /tmp/* \
-    /var/tmp/*
+apt-get update && apt-get install -y libnss3 libnotify4 libsecret-1-0 libgbm1 libasound2 fonts-wqy-zenhei gnutls-bin libglib2.0-dev libdbus-1-3 libgtk-3-0 libxss1 libxtst6 libatspi2.0-0 libx11-xcb1 ffmpeg unzip jq curl
+apt autoremove -y
+apt clean
+rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 # 安装Linux QQ
 curl -o linuxqq.deb https://dldir1.qq.com/qqfile/qq/QQNT/Linux/QQ_3.2.9_240606_arm64_01.deb
